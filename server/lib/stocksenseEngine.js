07ae8154b496e8074a9c7d9fa21990af67c9945e
tenlabs01 <tenlabs01@gmail.com>
2026-03-01 00:06:02 +0530

    stocksenseEngine.js

--- server/lib/stocksenseEngine.js
+++ server/lib/stocksenseEngine.js
@@ -139,7 +139,7 @@ export async function analyzeStockSensePortfolio({
     body: JSON.stringify({
       model,
       temperature: 0.2,
-      max_tokens: 150000,
+      max_tokens: 64000,
       system: STOCKSENSE_RULES_CONTEXT,
       messages: [{ role: "user", content: userPrompt }],
     }),
